3f4035780f5023016aac4b64b422c1f412d9e74b
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-14 12:55:10 +0530

    Implement user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    unless @user.save
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
