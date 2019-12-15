b720d2c4240450106c8559417d265bd5f95ce751
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-15 14:30:53 +0530

    Handle unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::API
+  include Pundit
   include Authenticable
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :unauthorized
+
+  private
+
+  def unauthorized
+    render :unauthorized, status: :unprocessable_entity
+  end
 end
---
+++ app/views/application/unauthorized.json.jbuilder
@@ -0,0 +1,4 @@
+json.error do
+  json.type "Unauthorized"
+  json.message "You don't have permission for this action."
+end
