795e33667f573a8846d604cd044e2ad651bfe550
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-15 20:38:20 +0530

    Send share id for easy deletion

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
   has_many :shares, dependent: :delete_all
+  has_many :contributing_shares, lambda { where(permission: :contributing) }, class_name: :Share
+  has_many :reading_shares, lambda { where(permission: :reading) }, class_name: :Share
   has_many :interactors, through: :shares, source: :user
   has_many :contributors, lambda { where("shares.permission": :contributing) }, {
     through: :shares,
@@ -10,6 +12,7 @@ class Note < ApplicationRecord
     through: :shares,
     source: :user
   }
+
 
   def share(user, permission)
     shares.find_or_initialize_by(user: user).tap do |share|
