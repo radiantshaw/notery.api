fe740f587db31acc3bd609448795f5f9e8676543
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-15 12:45:08 +0530

    Create shares table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_065421) do
+ActiveRecord::Schema.define(version: 2019_12_15_071049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2019_12_15_065421) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "shares", force: :cascade do |t|
+    t.integer "type"
+    t.bigint "user_id", null: false
+    t.bigint "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_shares_on_note_id"
+    t.index ["user_id"], name: "index_shares_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -32,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_12_15_065421) do
   end
 
   add_foreign_key "notes", "users"
+  add_foreign_key "shares", "notes"
+  add_foreign_key "shares", "users"
 end
