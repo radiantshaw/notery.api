da28befb4caf97a69f9b7f7c348f72c19185d84c
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-16 16:17:29 +0530

    Add CORS functionality

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,15 @@ module Notery
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", {
+          headers: :any,
+          methods: %i(get post options put delete)
+        }
+      end
+    end
   end
 end
